819b015230ec88b7ba828f67f55793320f3b3bb8
ben ord <bord@geodecisions.com>
2017-05-29 21:49:50 -0400

    working on image slider dynamic resize

--- js/script.js
+++ js/script.js
@@ -1,40 +1,54 @@
+debugger;
+resizeDivs();
+window.onresize = resizeDivs;
+
 var imageUrls = [
-            '../images/carousel/hatstand16x9.jpg',
-            '../images/carousel/lampstandtabletiny.jpg',
-            '../images/carousel/officetiny.jpg',
-            '../images/carousel/office2tiny.jpg'
-        ];
-        var imageIndex = -1;
-        var imageAElement = document.getElementById('imageA');
-        var imageBElement = document.getElementById('imageB');
-        //start immediately
-        switchImage();
-        //run every n milliseconds
-        setInterval(switchImage, 8000);
+    './images/carousel/hatstand16x9.jpg',
+    './images/carousel/lampstandtabletiny.jpg',
+    './images/carousel/officetiny.jpg',
+    './images/carousel/office2tiny.jpg'
+];
 
-        function switchImage() {
-            imageIndex = imageIndex + 1;
-            if (imageIndex >= imageUrls.length) {
-                imageIndex = 0;
-            }
-            var nextImageIndex = imageIndex + 1;
-            if (nextImageIndex >= imageUrls.length) {
-                nextImageIndex = 0;
-            }
-            var imageUrl = imageUrls[imageIndex];
-            var nextImageUrl = nextImageIndex;
-            var visibleElement;
-            var hiddenElement;
-            if (imageIndex % 2 === 0) {
-                visibleElement = imageAElement;
-                hiddenElement = imageBElement;
-            } else {
-                visibleElement = imageBElement;
-                hiddenElement = imageAElement;
-            }
+var imageIndex = -1;
+var imageAElement = document.getElementById('imageA');
+var imageBElement = document.getElementById('imageB');
 
-            visibleElement.src = imageUrl;
-            visibleElement.className = 'show';
+//start immediately
+switchImage();
 
-            hiddenElement.className = 'hide';
-        }
+//run every n milliseconds
+setInterval(switchImage, 8000);
+
+function switchImage() {
+    imageIndex = imageIndex + 1;
+    if (imageIndex >= imageUrls.length) {
+        imageIndex = 0;
+    }
+    var nextImageIndex = imageIndex + 1;
+    if (nextImageIndex >= imageUrls.length) {
+        nextImageIndex = 0;
+    }
+    var imageUrl = imageUrls[imageIndex];
+    var nextImageUrl = nextImageIndex;
+    var visibleElement;
+    var hiddenElement;
+    if (imageIndex % 2 === 0) {
+        visibleElement = imageAElement;
+        hiddenElement = imageBElement;
+    } else {
+        visibleElement = imageBElement;
+        hiddenElement = imageAElement;
+    }
+
+    visibleElement.src = imageUrl;
+    visibleElement.className = 'show';
+
+    hiddenElement.className = 'hide';
+}
+
+function resizeDivs() {
+    var innerWidth = document.body.innerWidth;
+    var innerHeight = innerWidth / 1.77;
+    relimagecontainer.style.setProperty('--element-height', innerHeight);
+    relimagecontainer.style.setProperty('--element-width', innerWidth);
+}
