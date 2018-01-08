499b9b490b9b6bbd8b5b43e16facef3e63dd3ca7
Ord, Benjamin L <bord@GFNET.com>
2018-01-08 15:04:37 -0500

    Offline work  containing working carousel

--- js/script.js
+++ js/script.js
@@ -1,54 +1,41 @@
-debugger;
 resizeDivs();
-window.onresize = resizeDivs;
+window.addEventListener('resize', resizeDivs);
+//window.addEventListener('scroll', headerScroll);
 
-var imageUrls = [
-    './images/carousel/hatstand16x9.jpg',
-    './images/carousel/lampstandtabletiny.jpg',
-    './images/carousel/officetiny.jpg',
-    './images/carousel/office2tiny.jpg'
-];
+// function headerScroll() {
+//     debugger;
+//     var scrollTop = $(window).scrollTop();
+//     var height = 75;
+//     var styleSheet = document.styleSheets[3];
+//     var x = ((1 - (height - scrollTop) / height) * 0.8) + 0.3;
+//     styleSheet.cssRules[3].style.backgroundColor = 'rgba (248, 248, 248, ' + x + ')';
+// }
 
-var imageIndex = -1;
-var imageAElement = document.getElementById('imageA');
-var imageBElement = document.getElementById('imageB');
+$(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
 
-//start immediately
-switchImage();
+    $('.nav-background').css({
+        backgroundColor: function() {
+            var height = 75;
+            var x = ((1 - (height - scrollTop) / height) * 0.8) + 0.3;
+            return 'rgba (248, 248, 248, ' + x + ')';
+        }
+    });
+});
 
-//run every n milliseconds
-setInterval(switchImage, 8000);
-
-function switchImage() {
-    imageIndex = imageIndex + 1;
-    if (imageIndex >= imageUrls.length) {
-        imageIndex = 0;
-    }
-    var nextImageIndex = imageIndex + 1;
-    if (nextImageIndex >= imageUrls.length) {
-        nextImageIndex = 0;
-    }
-    var imageUrl = imageUrls[imageIndex];
-    var nextImageUrl = nextImageIndex;
-    var visibleElement;
-    var hiddenElement;
-    if (imageIndex % 2 === 0) {
-        visibleElement = imageAElement;
-        hiddenElement = imageBElement;
-    } else {
-        visibleElement = imageBElement;
-        hiddenElement = imageAElement;
-    }
-
-    visibleElement.src = imageUrl;
-    visibleElement.className = 'show';
-
-    hiddenElement.className = 'hide';
-}
 
 function resizeDivs() {
-    var innerWidth = document.body.innerWidth;
-    var innerHeight = innerWidth / 1.77;
-    relimagecontainer.style.setProperty('--element-height', innerHeight);
-    relimagecontainer.style.setProperty('--element-width', innerWidth);
+    var innerWidth = window.innerWidth;
+    var styleSheet = document.styleSheets[3];
+    styleSheet.cssRules[9].style.height = innerWidth / 1.777777777777778;
+    styleSheet.cssRules[7].style.height = innerWidth / 1.777777777777778;
+    styleSheet.cssRules[10].style.height = innerWidth / 1.777777777777778;
+    // var carousel = document.getElementsByClassName(carousel);
+    // var item = document.getElementsByClassName(item);
+    // for(var i = 0; i < carousel.length; i++) {
+    //     carousel[i].style.height = innerHeight;
+    // }
+    // for(var j = 0; j < item.length; j++) {
+    //     item[j].style.height = innerHeight;
+    // }
 }
