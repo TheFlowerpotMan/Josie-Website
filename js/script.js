bd5ee080600dd47beed8c2a297780ad71225e962
Ord, Benjamin L <bord@GFNET.com>
2018-01-09 14:54:11 -0500

    Fixed hover issue and reorganized thumbnails

--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@ $(window).scroll(function() {
     var height = 0;
     var x = 0;
 
-    $('.nav-background').css({
-        "background-color": function() {
-            height = 75;
-            x = ((1 - (height - scrollTop) / height) * 0.08) + 0.3; 
-            return "rgba(248, 248, 248, " + x + ")";
-        }
-    });
+    if(scrollTop === 0) {
+        $('.nav-background').css({"background-color": "rgba(248, 248, 248, 0.0)"})
+    } else {
+        $('.nav-background').css({
+            "background-color": function() {
+                height = 75;
+                x = ((1 - (height - scrollTop) / height) * 0.08) + 0.1; 
+                return "rgba(248, 248, 248, " + x + ")";
+            }
+        });
+    }
 });
 
 
